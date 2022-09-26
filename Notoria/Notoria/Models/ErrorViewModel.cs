46efdb307cc9bcdbcb4a4ca9e65b5e43971da099
fer-br-tiger <fernando.gonzalez.261196@gmail.com>
2022-09-26 13:55:13 -0300

    Primer commit proyecto

---
+++ Notoria/Notoria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Notoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
